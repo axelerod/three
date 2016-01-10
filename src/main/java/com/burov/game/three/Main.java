684b6d756b6f1604afbb043cd6fa78723126c410
Oleksii Burov <aleksei.burov@gmail.com>
2016-01-10 16:30:24 +0200

    #8 Add ability to start client and server.

--- src/main/java/com/burov/game/three/Main.java
+++ src/main/java/com/burov/game/three/Main.java
@@ -5,6 +5,24 @@ import org.springframework.boot.SpringApplication;
 
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        String mode = "client";
+        if (args.length == 1) {
+            mode = args[0];
+        }
+
+        switch (mode) {
+            case "client":
+                return;
+            case "server":
+                SpringApplication.run(ServerApplication.class, args);
+                break;
+            default:
+                System.out.println("Usage: java -jar three.jar [<mode>]");
+                System.out.println("where <mode>: 'client' or 'server'");
+                System.out.println("if mode is skipped, client by default is started");
+                break;
+        }
+
     }
 }
+
