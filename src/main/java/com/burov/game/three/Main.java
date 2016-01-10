b3d2a9b92f754b8a9f27669c4e6115dc8703a409
Oleksii Burov <aleksei.burov@gmail.com>
2016-01-10 15:27:52 +0200

    #8 Renaming of app class

--- src/main/java/com/burov/game/three/Main.java
+++ src/main/java/com/burov/game/three/Main.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer{
+public class Main extends SpringBootServletInitializer{
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
